9b8a35a078e2a3e154cd8303b059b4b8b043e5df
emtekcan <105928904+emtekcan@users.noreply.github.com>
2022-11-17 11:04:55 +0000

    Defined interface for program counter

---
+++ pcreg.sv
@@ -0,0 +1,17 @@
+module pcreg (
+    input logic clk,
+    input logic rst,
+    input logic[11:0] ImmOp,
+    input logic PCsrc,
+
+    output logic[31:0] PC
+);
+(
+    logic[31:0] next_PC;
+    logic[31:0] inc_PC;
+    logic[31:0] branch_PC;
+
+    // assign branch_PC = PC + ImmOp;
+    // assign inc_PC = PC + 4;
+    // assign next_PC = PCsrc ? branch_PC : inc_PC;
+)
