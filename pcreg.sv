a8d2c85795f61330c1a4a99c2bcc474e24064c47
emtekcan <105928904+emtekcan@users.noreply.github.com>
2022-11-17 11:29:11 +0000

    Added register and assign statements to PC

--- pcreg.sv
+++ pcreg.sv
@@ -11,7 +11,15 @@ module pcreg (
     logic[31:0] inc_PC;
     logic[31:0] branch_PC;
 
-    // assign branch_PC = PC + ImmOp;
-    // assign inc_PC = PC + 4;
-    // assign next_PC = PCsrc ? branch_PC : inc_PC;
+    assign branch_PC = PC + ImmOp;
+    assign inc_PC = PC + 4;
+    assign next_PC = PCsrc ? branch_PC : inc_PC;
+
+    always_ff(posedge clk, posedge rst) begin
+        if(rst) PC <= 0;
+        else PC <= next_PC;
+    end
+
+
 )
+endmodule
