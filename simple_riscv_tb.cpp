ada4ef6398ccc3ae37b49b7fc5b057b61c4a0320
lemon-gith <pierce.wiegerling2@gmail.com>
2022-11-17 11:29:13 +0000

    actually copied other file in

--- simple_riscv_tb.cpp
+++ simple_riscv_tb.cpp
@@ -1,1 +1,48 @@
-#include "Verilated.h"
+#include "Vsth.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "../vbuddy.cpp"
+
+
+int main(int argc, char **argv, char **env){
+  int cyc;
+  int clock;
+
+  Verilated::commandArgs(argc, argv);
+
+  Vsth *top = new Vsth;
+
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *tfp = new VerilatedVcdC;
+
+  top->trace(tfp, 99);
+  tfp->open("sth.vcd");
+
+  if (vbdOpen("../vbuddy.cfg") != 1) return -1;
+  vbdHeader("T2-137F :)");
+  vbdSetMode(1);
+
+  top->clk = 1;
+  top->en = 1;
+
+  for (cyc = 0; cyc < 10000; cyc++){
+      if ((vbdValue() == 51) || (cyc < 2)) top->rst = 1;
+      else top->rst = 0;
+      top->en = vbdFlag();
+
+      for (clock = 0; clock < 2; clock++){
+          tfp->dump(2 * cyc + clock);
+          top->clk = !top->clk;
+          top->eval();
+      }
+
+      vbdBar(top->data_out & 0xFF);
+      vbdCycle(cyc);
+
+      if ((Verilated::gotFinish()) || (vbdGetkey()=='x')) exit(0);
+  }
+
+  tfp->close();
+  vbdClose();
+  exit(0);
+}
