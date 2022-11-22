a36a6c43625f3345f19c0d326ea8b2fc62fc18ce
emtekcan <105928904+emtekcan@users.noreply.github.com>
2022-11-22 17:30:01 +0000

    Added Control Unit and Sign Extend unit

---
+++ ctrlu.sv
@@ -0,0 +1,34 @@
+module ctrlu(
+    input logic EQ,
+    input logic [6:0] op;
+    input logic [2:0] funct3;
+    input logic funct7b5;
+    output logic RegWrite,
+    output logic [2:0] ALUctrl,
+    output logic ALUsrc,
+    output logic [1:0] ImmSrc,
+    output logic PCsrc
+);
+logic branch;
+logic [7:0] ctrl;
+assign {Regwrite, ImmSrc, ALUsrc, ALUctrl, branch} = ctrl;
+assign PCsrc = branch & (~EQ); //branch if not equals
+
+always_comb begin
+    case(op)
+        7'b0010011: //addi
+            case(funct3)
+                3'b000: ctrl = 8'b10010000;
+                default: ctrl = 8'b1001xxx0;
+            endcase
+        7'b1100011: //bne
+            case(funct3)
+                3'b001: ctrl = 8'b01000011;
+                default: ctrl = 8'b0100xxx1;
+            endcase
+
+        default: ctrl = 8'bxxxxxxxx;
+    endcase
+end
+
+endmodule
---
+++ extend.sv
@@ -0,0 +1,14 @@
+module extend(
+    input logic[1:0] ImmSrc,
+    input logic[31:7] Instr,
+    output logic[31:0] ImmOp
+);
+
+always_comb
+    case(ImmSrc)
+        2'b00: ImmOp = {{20{Instr[31]}}, Instr[31:20]};
+        2'b10: ImmOp = {{20{Instr[31]}}, Instr[7], Instr[30:25], Instr[11:8], 1'b0};
+        default: ImmOp = 32'bx;
+    endcase
+
+endmodule
