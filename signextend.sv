e15145c7dbc978e9a5be71036c63721946621fc0
ithanigaikumar <107815119+ithanigaikumar@users.noreply.github.com>
2022-11-23 12:22:08 +0000

    added fixed alu

---
+++ signextend.sv
@@ -0,0 +1,3 @@
+// split up bits so specific bits go to the control unit,rs1,rs2,rd and into sign extend block;
+//split bits according to the add instruction
+//use RD[31:19] for sign extension
--- sreg.sv
+++ sreg.sv
@@ -1,30 +1,36 @@
-module sreg(
+module sreg#(
     parameter ADDRESS_WIDTH=5,
               DATA_WIDTH=32
 )(
     input logic clk,
-    input logic[ADDRESS_WIDTH-1:0] rs1,
-    input logic[ADDRESS_WIDTH-1:0] rs2,
-    input logic [ADDRESS_WDITH-1:0]rd,
-    input logic RegWrite,
-    input logic WD3,
+    input logic[ADDRESS_WIDTH-1:0] ad1,
+    input logic[ADDRESS_WIDTH-1:0] ad2,
+    input logic [ADDRESS_WDITH-1:0]ad3,
+    input logic WE3,
+    input logic [DATA_WIDTH-1:0] WD3,
     output logic[DATA_WIDTH-1:0] ALUop1,
-    output logic[DATA_WIDTH-1:0] regOp2
-    output logic a0
+    output logic[DATA_WIDTH-1:0] regOp2,
+    output logic [ADDRESS_WIDTH-1:0]a0
 );
 
+//TEMPORARY REFERNEC ETABLE
+//ad3 determines which register the ALUout result writes to
 logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
 
-
-initial begin 
-    $display("Loading rom.");
-    $readmemh("rom.mem",rom_array)
-end;
-
+//link between ALUout and WD3
+//SET POSITION 0 TO 0
+//sequential write
 always_ff@(posedge clk) begin
-    ALUop1<=rom_array[rs1];
-    regOp2<=rom_array[rs2];
-    a0<=a0;
+    if(WE3)
+    rom_array[ad3]<=WD3;
+    
 end 
 
+//combinatorial read ->combinatorial should be equal
+always_comb
+    begin
+    ALUop1=rom_array[ad1];
+    regOp2=rom_array[ad2];
+    a0<rom_array[5'b00101]; // this is rom_array 10: //A0 is register 10: abi name, risc v abi: application binary interfcae
+    end
 endmodule
