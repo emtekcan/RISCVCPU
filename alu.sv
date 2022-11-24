39890342b7985c6e98ed90b241901b7a2ad5cd1c
ithanigaikumar <107815119+ithanigaikumar@users.noreply.github.com>
2022-11-24 08:30:31 +0000

    modify alu

--- alu.sv
+++ alu.sv
@@ -12,9 +12,13 @@ module alu #(
 
 always_comb //always comb //case add aluctrl
     case(ALUctrl)
-    1'b0:ALUout = ALUop1+ALUop2;
+    1'b0:begin
+        ALUout = ALUop1+ALUop2;
+        EQ= 0;
+    end
+
      //need rs1 and rs2 to be the same.
-    1'b1:if(ALUop1 == ALUop2) EQ =1; //
+    1'b1:EQ=(ALUop1 == ALUop2) ; //
     default: ALUout = 0; //what should default case
     endcase
 
